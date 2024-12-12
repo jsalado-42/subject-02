f80d3cae820e13233aa3e4a278046c18ff095596
jsalado- <jsalado-@students.42malaga.com>
2024-12-12 16:46:09 +0100

    minishell modified

--- minishell.c
+++ minishell.c
@@ -6,11 +6,16 @@
 /*   By: jsalado- <jsalado-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 16:42:47 by jsalado-          #+#    #+#             */
-/*   Updated: 2024/12/12 16:43:09 by jsalado-         ###   ########.fr       */
+/*   Updated: 2024/12/12 16:45:13 by jsalado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+
+void	ft_putstr(char *str)
+{
+	return ;
+}
 
 int	main(void)
 {
